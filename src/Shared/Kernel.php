69f9d3082b3a9f7d440e7197b644057291e9b6a7
Yaroslav Kravtsov <yaroslav.kravtsov@debricked.com>
2023-02-06 21:02:52 +0100

    feat(#1): add phpinsights

--- src/Shared/Kernel.php
+++ src/Shared/Kernel.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Shared;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
