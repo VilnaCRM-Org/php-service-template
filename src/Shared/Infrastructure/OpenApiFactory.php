5f2ea5bd2c349311b7218a81de4441d2c2d14404
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2023-12-25 12:28:58 +0200

    feat(#1): add fuzzing config

---
+++ src/Shared/Infrastructure/OpenApiFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Shared\Infrastructure;
+
+use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\OpenApi\Model;
+use ApiPlatform\OpenApi\Model\Response;
+use ApiPlatform\OpenApi\OpenApi;
+
+class OpenApiFactory implements OpenApiFactoryInterface
+{
+    public function __construct(private OpenApiFactoryInterface $decorated)
+    {
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+
+        $openApi = $openApi->withServers([new Model\Server('https://localhost')]);
+
+        return $openApi;
+    }
+}
