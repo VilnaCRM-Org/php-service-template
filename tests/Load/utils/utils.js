4282613f5b576c4db1b03442241fda298a5372de
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-08-01 20:58:20 +0300

    feat(#30): delete redundant methods in utils

--- tests/Load/utils/utils.js
+++ tests/Load/utils/utils.js
@@ -23,73 +23,17 @@ export default class Utils {
         }
     }
 
-    getBaseUrl() {
-        return this.baseUrl;
-    }
-
     getBaseHttpUrl() {
         return this.baseHttpUrl;
-    }
-
-    getBaseGraphQLUrl() {
-        return this.baseGraphQLUrl;
-    }
-
-    getGraphQLIdPrefix() {
-        return this.graphQLIdPrefix;
-    }
-
-    getJsonHeader() {
-        return {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-    }
-
-    getMergePatchHeader() {
-        return {
-            headers: {
-                'Content-Type': 'application/merge-patch+json',
-            },
-        };
-    }
-
-    getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     getCLIVariable(variable) {
         return `${__ENV[variable]}`;
     }
 
-    checkUserIsDefined(user) {
-        check(user, {'user is defined': (u) => u !== undefined});
-    }
-
-    generateUser(){
-        const email = `${faker.number.int32()}${faker.person.email()}`;
-        const initials = faker.person.name();
-        const password = faker.internet.password(true, true, true, false, false, 60);
-
-        return {
-            email,
-            password,
-            initials,
-        };
-    }
 
    checkResponse(response, checkName, checkFunction) {
         check(response, {[checkName]: (res) => checkFunction(res)});
     }
 
-    registerUser(user){
-        const payload = JSON.stringify(user);
-
-        return http.post(
-            this.getBaseHttpUrl(),
-            payload,
-            this.getJsonHeader()
-        );
-    }
 }
