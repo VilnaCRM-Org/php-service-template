7578a9f9d5dade0544b1563e2fcdf9329cde9016
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-08-15 11:47:05 +0300

    refactor(#30): add readonly instead of private&setters

--- src/Shared/Application/OpenApi/Builder/Header.php
+++ src/Shared/Application/OpenApi/Builder/Header.php
@@ -4,39 +4,14 @@ declare(strict_types=1);
 
 namespace App\Shared\Application\OpenApi\Builder;
 
-final class Header
+final readonly class Header
 {
     public function __construct(
-        private readonly string $name,
-        private readonly string $description,
-        private readonly string $type,
-        private readonly string $format,
-        private readonly string $example
+        public string $name,
+        public string $description,
+        public string $type,
+        public string $format,
+        public string $example
     ) {
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    public function getType(): string
-    {
-        return $this->type;
-    }
-
-    public function getFormat(): string
-    {
-        return $this->format;
-    }
-
-    public function getExample(): string
-    {
-        return $this->example;
     }
 }
