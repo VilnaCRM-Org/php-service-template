fcb42d5ebe3126a9d1c4a9d1426fc2f6f703deb1
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-09-04 16:41:15 +0300

    refactor(#30): refactor outputting info in load tests

--- tests/Load/run-load-tests.sh
+++ tests/Load/run-load-tests.sh
@@ -3,13 +3,15 @@ set -e -x
 
 echo "Running load tests..."
 echo "Current directory: $(pwd)"
+echo "----------------------------------------"
 echo "Listing files:"
 ls -l
+echo "----------------------------------------"
 
 LOAD_TEST_SCENARIOS=$(./tests/Load/get-load-test-scenarios.sh)
 echo "Scenarios: $LOAD_TEST_SCENARIOS"
 
 for scenario in $LOAD_TEST_SCENARIOS; do
-  echo "Running scenario: $scenario"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Running scenario: $scenario"
   ./tests/Load/execute-load-test.sh "$scenario" true true true true all-
 done
