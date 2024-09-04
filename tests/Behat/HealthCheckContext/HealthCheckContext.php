2801cfcd036c539e1cd4bb5eb157417d8de1ad00
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-09-04 16:45:43 +0300

    refactor(#30): refactor behat

--- tests/Behat/HealthCheckContext/HealthCheckContext.php
+++ tests/Behat/HealthCheckContext/HealthCheckContext.php
@@ -4,23 +4,31 @@ declare(strict_types=1);
 
 namespace App\Tests\Behat\HealthCheckContext;
 
+use Aws\Sqs\SqsClient;
 use Behat\Behat\Context\Context;
+use Doctrine\Common\EventManager;
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver;
 use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Cache\Exception\CacheException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
-final class HealthCheckContext implements Context
+final class HealthCheckContext extends KernelTestCase implements Context
 {
-    private KernelInterface $kernel;
+    private KernelInterface $kernelInterface;
     private Response $response;
 
     public function __construct(
         KernelInterface $kernel
     ) {
-        $this->kernel = $kernel;
+        parent::__construct();
+        $this->kernelInterface = $kernel;
     }
 
     /**
@@ -28,7 +36,7 @@ final class HealthCheckContext implements Context
      */
     public function sendRequestTo(string $method, string $path): void
     {
-        $this->response = $this->kernel->handle(Request::create(
+        $this->response = $this->kernelInterface->handle(Request::create(
             $path,
             $method,
         ));
@@ -43,28 +51,76 @@ final class HealthCheckContext implements Context
     }
 
     /**
+     * @Then the response body should contain :text
+     */
+    public function theResponseBodyShouldContain(string $text): void
+    {
+        $responseContent = $this->response->getContent();
+        Assert::assertStringContainsString(
+            $text,
+            $responseContent,
+            "The response body does not contain the expected text: '{$text}'."
+        );
+    }
+
+    /**
      * @Given the cache is not working
      */
     public function theCacheIsNotWorking(): void
     {
-        $container = $this->kernel->getContainer();
+        /** @var CacheInterface|MockObject $cacheMock */
+        $cacheMock = $this->createMock(CacheInterface::class);
 
-        $cacheTestDouble = new class() implements CacheInterface {
-            public function get(
-                string $key,
-                callable $callback,
-                ?float $beta = null,
-                ?array &$metadata = null
-            ): mixed {
-                throw new CacheException();
-            }
+        $cacheMock->method('get')
+            ->willThrowException(new CacheException('Cache is not working'));
 
-            public function delete(string $key): bool
-            {
-                return true;
-            }
-        };
+        $container = $this->kernelInterface->getContainer();
+        $container->set(CacheInterface::class, $cacheMock);
+    }
 
-        $container->set(CacheInterface::class, $cacheTestDouble);
+    /**
+     * @Given the database is not available
+     */
+    public function theDatabaseIsNotAvailable(): void
+    {
+        $driverMock = $this->createMock(Driver::class);
+
+        $connectionMock = $this->getMockBuilder(Connection::class)
+            ->setConstructorArgs([
+                [],
+                $driverMock,
+                new Configuration(),
+                new EventManager(),
+            ])
+            ->onlyMethods(['executeQuery'])
+            ->getMock();
+
+        $connectionMock->method('executeQuery')
+            ->willThrowException(new \Exception('Database is not available'));
+
+        $container = $this->kernelInterface
+            ->getContainer()
+            ->get('test.service_container');
+        $container->set(Connection::class, $connectionMock);
+    }
+
+    /**
+     * @Given the message broker is not available
+     */
+    public function theMessageBrokerIsNotAvailable(): void
+    {
+        $sqsClientMock = $this->createMock(SqsClient::class);
+
+        $sqsClientMock->method('__call')
+            ->willThrowException(
+                new \Exception(
+                    'Message broker is not available'
+                )
+            );
+
+        $container = $this->kernelInterface
+            ->getContainer()
+            ->get('test.service_container');
+        $container->set(SqsClient::class, $sqsClientMock);
     }
 }
