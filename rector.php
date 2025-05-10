d95bc4e9af8c29d22877cdb0d3f640d5d51a2d1e
Nikita Grinyov <nikita3grynov@gmail.com>
2025-05-10 17:36:00 +0300

    fix for phpinsights

--- rector.php
+++ rector.php
@@ -3,16 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php84\Rector\MethodCall\NewMethodCallWithoutParenthesesRector;
-use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
-use
-    Rector\Php83\Rector\FuncCall\RemoveGetClassGetParentClassNoArgsRector;
 use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Php83\Rector\FuncCall\RemoveGetClassGetParentClassNoArgsRector;
+use Rector\Php84\Rector\MethodCall\NewMethodCallWithoutParenthesesRector;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
+use Rector\Symfony\Symfony28\Rector\MethodCall\GetToConstructorInjectionRector;
 use Rector\Symfony\Symfony30\Rector\MethodCall\StringFormTypeToClassRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
-use Rector\Symfony\Symfony28\Rector\MethodCall\GetToConstructorInjectionRector;
+
 
 return RectorConfig::configure()
     ->withPaths([
