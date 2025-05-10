6d9feeab8d33af81f71792ef53669b8f96cbb6ec
Nikita Grinyov <nikita3grynov@gmail.com>
2025-05-10 17:26:02 +0300

    fixing coderabbits warnings

--- rector.php
+++ rector.php
@@ -3,16 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
-use Rector\Php83\Rector\FuncCall\RemoveGetClassGetParentClassNoArgsRector;
 use Rector\Php84\Rector\MethodCall\NewMethodCallWithoutParenthesesRector;
 use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
-use Rector\Symfony\CodeQuality\Rector\ClassMethod\ResponseReturnTypeControllerActionRector;
-use Rector\Symfony\Symfony28\Rector\MethodCall\GetToConstructorInjectionRector;
+use
+    Rector\Php83\Rector\FuncCall\RemoveGetClassGetParentClassNoArgsRector;
+use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\Symfony\Symfony30\Rector\MethodCall\StringFormTypeToClassRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+use Rector\Symfony\Symfony28\Rector\MethodCall\GetToConstructorInjectionRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -30,7 +30,6 @@ return RectorConfig::configure()
         GetToConstructorInjectionRector::class,
         NewMethodCallWithoutParenthesesRector::class,
         RemoveGetClassGetParentClassNoArgsRector::class,
-        ResponseReturnTypeControllerActionRector::class,
         StringFormTypeToClassRector::class,
     ])
     ->withSymfonyContainerXml(
